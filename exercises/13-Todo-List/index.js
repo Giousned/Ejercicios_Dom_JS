45089bb254006c7d6f4911ec7c0fb3124f80d1d7
Giousned <guille_ranza@hotmail.com>
2023-02-21 11:50:52 +0000

    Otra version y mas pruebas, pero sin pasar test aun

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,5 +1,76 @@
 // Your code here
 let spanElement = document.getElementsByTagName("span");
+let addToDoElement = document.getElementById("addToDo");
+
+
+// Create a className "close" and append it to each list item
+for (let i = 0; i < spanElement.length; i++) {
+    spanElement[i].addEventListener("click", deleteTask);
+  }
+
+
+// // Add a "checked" symbol when clicking on a list item
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//   if (ev.target.tagName === 'LI') {
+//     ev.target.classList.toggle('checked');
+//   }
+// }, false);
+
+
+// Create a new list item when clicking "Enter"
+addToDoElement.addEventListener("keypress", function (e) {
+  //your code here
+  if (e.key === "Enter") {
+    addTask();
+  }
+});
+
+function addTask() {
+
+  let li = document.createElement("li");
+
+  li.innerHTML = `<span><i class="fa fa-trash"></i></span>`;
+
+  let inputValue = document.getElementById("addToDo").value;
+  let t = document.createTextNode(inputValue);
+
+  li.appendChild(t);
+
+  document.querySelector("ul").appendChild(li);
+
+  addToDoElement.value = "";
+
+  for (let i = 0; i < spanElement.length; i++) {
+    spanElement[i].addEventListener("click", deleteTask);
+  }
+}
+
+// Click on a trashclose button to hide the current list item
+function deleteTask() {
+  this.parentElement.remove(this)
+}
+
+/* PROBAR INTERNET
+const addForm = document.querySelector('.add');
+ 
+addForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const newToDo = addForm.add.value;
+  console.log(newToDo);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('div').on('click', '[data-fa-i2svg]', function () {
+    alert('You clicked the icon itself');
+  });
+  });	
+}
+e.target.classList.contains("fa-search")
+*/
+
+/* OTRA FORMA INTERNET
+let spanElement = document.getElementsByTagName("span");
 let close = document.getElementsByClassName("close");
 let addToDoElement = document.getElementById("addToDo");
 
@@ -29,9 +100,9 @@ for (let i = 0; i < close.length; i++) {
 // Create a new list item when clicking "Enter"
 addToDoElement.addEventListener("keypress", function(e) {
 //your code here
-		if (e.key === "Enter"){
-			newElement();
-		}
+    if (e.key === "Enter"){
+      newElement();
+    }
 });
 
 function newElement() {
@@ -62,64 +133,53 @@ function newElement() {
     }
   }
 }
-
-
-/* PROBAR INTERNET
-const addForm = document.querySelector('.add');
- 
-addForm.addEventListener('submit', e => {
-	e.preventDefault();
-	const newToDo = addForm.add.value;
-	console.log(newToDo);
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-	$('div').on('click', '[data-fa-i2svg]', function () {
-	  alert('You clicked the icon itself');
-	});
-  });	
-}
-
-for (let item of listLi){
-	trashIcon.addEventListener("click", () => handleRemoveElement(item));
-}
-		deleteTask(item);
-	parentNode.removeChild(e);
-
-for (var i = 0; i <= lista.children.length -1; i++) {
-	lista.children[i].addEventListener("click", function(){
-		this.parentNode.removeChild(this);
-	});
-} e.target.classList.contains("fa-search")
 */
 
-/* ESTO MEDIO FUNCIONA
+
+/* OTRA FORMA
 let addToDo = document.getElementById("addToDo");
 let ulElement = document.getElementById("myClass");
 let trashIcon = document.getElementsByClassName("fa-trash");
 let listLi = document.querySelectorAll("li");
 
 function addTask () {
-	let li = document.createElement("li");
-		li.innerHTML = `<span><i class="fa fa-trash"></i></span>${addToDo.value}`;
-		ulElement.appendChild(li);
+  let li = document.createElement("li");
+    li.innerHTML = `<span><i class="fa fa-trash"></i></span>${addToDo.value}`;
+    ulElement.appendChild(li);
 }
 
 addToDo.addEventListener("keypress", function(e) {
-	//your code here
-	if (e.key === "Enter"){
-		addTask();
-	}
+  //your code here
+  if (e.key === "Enter"){
+    addTask();
+  }
 });
 
+  for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", function() {
+      // let div = this.parentElement;
+      // div.style.display = "none";
+      close[i].parentElement.style.display = "none";
+    })
+  }
+
+    for (let i = 0; i < close.length; i++) {
+    close[i].addEventListener("click", function() {
+      this.parentElement.remove(this);   // FUNCIONA
+    })
+*/
+
+
+/* ESTO MEDIO FUNCIONA
+-------------------
 function deleteTask () {
-	for (let i = 0; i < listLi.length ; i++) {
-		// trashIcon[i].childNodes[i].remove();
-		listLi[i].remove();
-	}
+  for (let i = 0; i < listLi.length ; i++) {
+    // trashIcon[i].childNodes[i].remove();
+    listLi[i].remove();
+  }
 }
 
 for (let i = 0; i < trashIcon.length ; i++) {
-	trashIcon[i].addEventListener("click", deleteTask);
+  trashIcon[i].addEventListener("click", deleteTask);
 }
 */
